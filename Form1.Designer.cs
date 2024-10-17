afdb2cb9d8ad48f9006f508e45ceff971e4f7f45
nguyenly124 <nguyenthily124da@gmail.com>
2024-10-17 15:40:23 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Font_dang_ky
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            Email = new TextBox();
+            Pass = new TextBox();
+            button_dangky = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(293, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 20);
+            label1.TabIndex = 0;
+            label1.Text = "ĐĂNG KÝ ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(117, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Email";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(118, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            label3.Click += label3_Click;
+            // 
+            // Email
+            // 
+            Email.Location = new Point(231, 91);
+            Email.Name = "Email";
+            Email.Size = new Size(317, 27);
+            Email.TabIndex = 3;
+            Email.TextChanged += textBox1_TextChanged;
+            // 
+            // Pass
+            // 
+            Pass.Location = new Point(233, 151);
+            Pass.Name = "Pass";
+            Pass.Size = new Size(315, 27);
+            Pass.TabIndex = 4;
+            Pass.UseSystemPasswordChar = true;
+            // 
+            // button_dangky
+            // 
+            button_dangky.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_dangky.Location = new Point(275, 235);
+            button_dangky.Name = "button_dangky";
+            button_dangky.Size = new Size(94, 29);
+            button_dangky.TabIndex = 5;
+            button_dangky.Text = "Đăng ký ";
+            button_dangky.UseVisualStyleBackColor = true;
+            button_dangky.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(672, 351);
+            Controls.Add(button_dangky);
+            Controls.Add(Pass);
+            Controls.Add(Email);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox Email;
+        private TextBox Pass;
+        private Button button_dangky;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Font_dang_ky
+{
+    using System.Text.RegularExpressions;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            string email = Email.Text;
+            string password = Pass.Text;
+
+            // Kiểm tra thông tin (có thể thêm điều kiện như kiểm tra rỗng)
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password) )
+                {
+                    MessageBox.Show("Vui lòng điền đầy đủ thông tin!");
+                    return;
+                }
+            if (!IsValidEmail(email))
+            {
+                MessageBox.Show("Email không hợp lệ! Vui lòng kiểm tra lại.");
+                return;
+            }
+            if (!IsValidPasword(password))
+            {
+                MessageBox.Show("Mật khẩu không hợp lệ. Yêu cầu tối thiểu 8 ký tự và phải bao gồm ít nhất 1 ký tự đặc biệt.");
+                return;
+            }
+            // Thêm logic để lưu thông tin vào cơ sở dữ liệu hoặc file
+            MessageBox.Show("Đăng ký thành công!");
+            }
+
+        private bool IsValidPasword(string password)
+        {
+            string pattern = @"^(?=.*[!@#$%^&*(),.?""{}|<>])[A-Za-z\d!@#$%^&*(),.?""{}|<>]{8,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email,pattern);
+        }
+    }
+}
